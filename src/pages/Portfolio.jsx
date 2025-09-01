1fa247e74757660935878ae474865abba91d638e
rhettbarton <rhett.bart@gmail.com>
2025-09-01 15:12:01 -0600

    Detect env for credentials vs IAM

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -70,11 +70,7 @@ function Portfolio() {
   // Get unique categories from products
   const categories = ['all', ...new Set(products.map(product => product.category))]
 
-  const openLightbox = (product, imageIndex = 0) => {
-    // Images are already loaded from DynamoDB files list
-    console.log('Opening lightbox for product:', product);
-    console.log('Product images from files list:', product.images);
-    
+  const openLightbox = (product, imageIndex = 0) => { 
     // Use images from the files list
     let images = product.images && product.images.length > 0 ? product.images : [];
     
